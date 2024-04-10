3334eb9bddbf5e1285d4c1b98097d042698d3e95
dev global <aynair123@gmail.com>
2024-04-09 18:01:09 -0700

    Commit Test 1

---
+++ Case.py
@@ -0,0 +1,4 @@
+class Case:
+    def __init__(self, caseName):
+        self.hashname = caseName
+        self.itemDenoms = dict({"Covert":0, "Classfied":0, "Restricted":0, "Milspec":0, "Extraordinary":0})
