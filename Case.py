65073d114b485e44202dabedadf01b2b85aa208d
dev global <aynair123@gmail.com>
2024-04-09 18:24:20 -0700

    Started Case Object

--- Case.py
+++ Case.py
@@ -1,4 +1,30 @@
+itemRarities = dict({
+    "eb4b4b": "Covert",
+    "d32ce6": "Classified",
+    "8847ff": "Restricted",
+    "4b69ff": "Milspec"
+})
+
+itemProbs = dict({
+    "Covert": 0.0058,
+    "Classified": 0.0288,
+    "Restricted": 0.1438,
+    "Milspec": 0.7193,
+    "Extraordinary": 0.0023
+})
+
+
 class Case:
     def __init__(self, caseName):
         self.hashname = caseName
-        self.itemDenoms = dict({"Covert":0, "Classfied":0, "Restricted":0, "Milspec":0, "Extraordinary":0})
+        self.itemDenoms = dict({
+            "Covert": 0,
+            "Classfied": 0,
+            "Restricted": 0,
+            "Milspec": 0,
+            "Extraordinary": 0
+        })
+        self.items = []
+        self.extraordinaries = []
+        self.expectedVal = 0
+
